58c9930b7c81a02ac718388788e052d612b0d6f9
youssef sameh <yousef17453@gmail.com>
2025-10-06 21:55:35 +0300

    initial commit - Day01 of Js

---
+++ 01-Fundamentals/starter/script.js
@@ -0,0 +1,81 @@
+/**********starter************/
+/* let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('youssef');
+let firstName = 'Youssef';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+//let new = test //reserved keyword
+//let 3years = 3 //cannot start with number
+//let jon&doe = 'JD' //cannot contain special characters except $ and _
+//let function = 27 //reserved keyword
+let $function = 27 //valid but not recommended
+let Pi = 3.14 //valid but not recommended
+let KALI = 'Linux' //valid but not recommended
+
+//assignment
+let country = 'Egypt';
+let continent = 'Africa';
+let population = 110;
+
+console.log(country);
+console.log(continent);
+console.log(population); 
+
+/**********02.Data Types************/
+
+/* let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Youssef');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); //bug 
+
+// assignment
+/* let country = 'Egypt';
+let continent = 'Africa';
+let population = 110;
+
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language); 
+
+/**********03.Let, Const and Var************/
+
+/*let age = 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear = 1990;
+//  //TypeError because const variable cannot be reassigned
+//const job;
+//  //SyntaxError because we cannot declare an empty const variable 
+*/
+
+
+
