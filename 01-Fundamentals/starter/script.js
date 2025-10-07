1a98c7bc580a39ec377239b4216536c3021a717a
youssef sameh <yousef17453@gmail.com>
2025-10-07 18:53:25 +0300

    finished fundamentals

--- 01-Fundamentals/starter/script.js
+++ 01-Fundamentals/starter/script.js
@@ -253,4 +253,14 @@ switch (day) {
 
 /***************13.Ternary Operator*****************************/
 
+/*
+//const age = 23;
+// age >= 18 ? console.log("i'd like to drink wine") :
+//     console.log("i'd like to drink water");
+//console.log(`i'd like to drink ${age >= 18 ? "wine" : "water"}`);
+
+const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? bill * 15 / 100 : bill * 20 / 100;
+console.log(`the bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+*/
 
