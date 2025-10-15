b09842be0c0173d2998dae9024c95bf646dee525
Youssef Sameh <yousef17453@gmail.com>
2025-10-15 06:51:05 +0300

    handled click events and classes

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log(document.querySelector(".message").textContent);
+document.addEventListener("keydown", function(e){
+	if(e.key!= "Escape") {
+		console.log("press Esc to quit");
+	}
+	else if(e.key === "Escape" && !modal.classList.contains("hidden")){
+		closeModal()
+	}
+});
