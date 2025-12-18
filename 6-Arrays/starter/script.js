9ae7c129e9ac7ca07b14da8efcc49fa6c9ba13a0
youssef sameh <114283304+youssefs20@users.noreply.github.com>
2025-12-18 22:51:31 +0200

    calcDisplaySummary (#2)
    * calcBalance function
    
    * calcDisplayFunction function
    
    * Update script

--- 6-Arrays/starter/script.js
+++ 6-Arrays/starter/script.js
@@ -94,6 +94,32 @@ function calcPrintBalance(movements) {
 
 calcPrintBalance(account1.movements);
 
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+
+  labelSumIn.textContent = `${incomes} EUR`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+
+  labelSumOut.textContent = `${Math.abs(out)} EUR`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      // console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+
+  labelSumInterest.textContent = `${interest} EUR`;
+};
+calcDisplaySummary(account1.movements);
+
 function createUsernames(accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
@@ -119,5 +145,4 @@ const currencies = new Map([
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
+////////////////////////////////////////////////
